348963c02112d796b001ab4f3e1d6f47d8f832a4
lihao <ablelh@163.com>
2018-12-29 20:15:54 +0800

    Initial Commit

---
+++ forwardInvocationDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  forwardInvocationDemo
+//
+//  Created by lihao on 2018/12/29.
+//  Copyright © 2018 lihao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
